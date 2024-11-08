ce58a5eb8819e6cfcf84bf70614a066c268b7e6e
SamLai228 <samgcc742@gmail.com>
2024-11-08 14:27:39 +0800

    update model used

--- app/app.py
+++ app/app.py
@@ -373,7 +373,7 @@ with tab2:
     current_dir = os.path.dirname(os.path.abspath(__file__))
 
     # 載入模型
-    model_path = os.path.join(current_dir, 'models', 'xgboost_new.pkl')
+    model_path = os.path.join(current_dir, 'models', 'balanced_random_forest_new.pkl')
     with open(model_path, 'rb') as f:
         model = pickle.load(f)
 
